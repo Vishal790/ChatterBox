7cc845bdbfefb3b23226c604f4ca982067ecdf88
Vishal Srivastava <vishal.vs690@gmail.com>
2024-06-20 21:19:22 +0530

    fixed root

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    rollupOptions: {
-      external: ['mongoose']
-    }
-  },
   server: {
     port: 4000,
   },
