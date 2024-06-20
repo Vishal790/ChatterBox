f63519da0eb8c70460e4ab67462866e963276166
Vishal Srivastava <vishal.vs690@gmail.com>
2024-06-20 21:05:44 +0530

    Fixed mongoose imports

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root:"src",
+   build: {
+    rollupOptions: {
+      external: ['mongoose']
+    }
+  },
   server: {
     port: 4000,
   },
