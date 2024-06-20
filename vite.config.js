352ec526eef21ff3a2b0e856889bd0756478d4e8
Vishal Srivastava <vishal.vs690@gmail.com>
2024-06-20 21:38:46 +0530

    fixed mongoose vercel

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: /^mongoose($|\/)/, // Externalize mongoose
-    },
-  },
   server: {
     port: 4000,
   },
